98bcfdca2b98b4ed27642bc9c3f88596be85dad0
Dan Wiebe <dnwiebe@gmail.com>
2016-03-12 14:33:20 -0500

    Custom validation

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -3,6 +3,7 @@ package controllers
 import config.Global
 import play.api.data._
 import play.api.data.Forms._
+import play.api.data.validation.{Valid, Invalid, Constraint}
 import play.api.libs.json.JsValue
 import play.api.mvc._
 import play.api.Play.current
@@ -40,10 +41,16 @@ trait GameController {
   private def makeUserDataForm: Form[UserData] = {
     Form (
       mapping (
-        "name" -> nonEmptyText
+        "name" -> nonEmptyText.verifying (scriptInjection)
       )(UserData.apply)(UserData.unapply)
     )
   }
+
+  private val scriptInjection: Constraint[String] = Constraint (None, Nil)({
+    case s: String if s.contains ("<script>") =>
+      Invalid ("Seriously? Script injection? From you? I thought we were past that.")
+    case _ => Valid
+  })
 }
 
 object GameController extends Controller with GameController {
